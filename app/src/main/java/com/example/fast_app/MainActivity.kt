92d0b8208058ed122462054583ece0cf4124e74d
sws2 <ahnjh1028@naver.com>
2020-10-28 16:11:41 +0900

    First Kotlin study

--- app/src/main/java/com/example/fast_app/MainActivity.kt
+++ app/src/main/java/com/example/fast_app/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(R.layout.support_simple_spinner_dropdown_item)
     }
 }
