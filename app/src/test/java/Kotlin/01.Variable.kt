73c38874e71a7b60f9d26e834a9c13bd9e15b4a1
sws2 <ahnjh1028@naver.com>
2020-10-28 15:36:10 +0900

    Initial commit

---
+++ app/src/test/java/Kotlin/01.Variable.kt
@@ -0,0 +1,19 @@
+package Kotlin
+
+//01 변수 선언
+//->상자
+//변수 선언하는 방법
+// 내 맘대로 원하는것을 넣을수 없는 상자->variable
+//한번 넣으면 바꿀수 없는 상자->Value
+//var/val 변수명 (상자) = 값(넣고 싶은 것)
+//Variable==var/value==val
+var num:Int = 10
+var hello:String = "hello"
+var point:Double = 3.4;
+val fix:Int = 20
+fun main(args:Array<String>){
+    print(num)
+
+
+
+}
