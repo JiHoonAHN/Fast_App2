ac1db2696945bb169cdb3cc9a7e75566631d5727
sws2 <ahnjh1028@naver.com>
2020-10-29 23:49:41 +0900

    type1

--- app/src/test/java/Kotlin/03.Pratice.kt
+++ app/src/test/java/Kotlin/03.Pratice.kt
@@ -3,7 +3,19 @@ package Kotlin
 var a = 1 + 2 + 3 + 4 + 5//연산의 결괏값을 변수에 넣어 줄 수 있다.
 var b = "1"
 var c = b.toInt()
+var d = b.toFloat()
+
+var e = "John"
+var f ="My name is $e Nice to meet you"
+
+
+//Null
+// - 존재하지 않는다.
 fun main(array:Array<String>)
 {
-
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(f)
 }
