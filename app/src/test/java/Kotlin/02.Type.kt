1b82704aa85a2be8b8cdf0bcb08bdf425185d816
sws2 <ahnjh1028@naver.com>
2020-10-29 23:02:53 +0900

    type2

--- app/src/test/java/Kotlin/02.Type.kt
+++ app/src/test/java/Kotlin/02.Type.kt
@@ -10,16 +10,16 @@ package Kotlin
 //Varialbe/Value 변수명 = 값
 //var/val 변수명 = 값
 var number:Int = 10
+var number1 : Int =20
+var  hello1 : String = "Hello"
+var point1:Double = 3.4
 fun main(array:Array<String>){
     number = 20
     //변수 선언법 2
     //var/val 변수 : 자료형 = 값
-    var number1 : Int =20
-    var  hello1 : String = "Hello"
-    var point1:Double = 3.4
-    print(number1)
-    print(hello1)
-    print(point1)
     print(number)
+    //Variable or Value
+    //-1 . 변하지 않는 갑이라면 Value2
+    //-2. 진짜 모르겠다!->Value
 
 }
