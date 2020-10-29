4d2c230377cac09769aa540fee48020f213ee310
sws2 <ahnjh1028@naver.com>
2020-10-29 15:36:29 +0900

    type

--- app/src/test/java/Kotlin/02.Type.kt
+++ app/src/test/java/Kotlin/02.Type.kt
@@ -1,2 +1,20 @@
 package Kotlin
 
+//02. 자료형
+//정수형 - > Long >Int>Short>Byte
+//실수형 - > Double >Float
+// 문자 -> Char
+//문자열 ->String
+//논리형(참True,거짓False) ->Boolean
+//변수 선언법
+//Varialbe/Value 변수명 = 값
+//var/val 변수명 = 값
+var number:Int = 10
+fun main(array:Array<String>){
+    number = 20
+    //변수 선언법 2
+    //var/val 변수 : 자료형 = 값
+    var number1 : Int =20
+    var  hello1 : String = "Hello"
+    var point1:Double = 3.4
+}
