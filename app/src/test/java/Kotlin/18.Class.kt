10074772298690f01fec672882d33fa3e2350670
sws2 <ahnjh1028@naver.com>
2020-11-02 23:41:30 +0900

    type1

--- app/src/test/java/Kotlin/18.Class.kt
+++ app/src/test/java/Kotlin/18.Class.kt
@@ -16,5 +16,47 @@ import java.util.ArrayList
 // - 설명서가 있어야 한다
 fun main(array:Array<String>)
 {
+    //클래스(설명서)를 통해서 실체를 만드는 방법
+    //-> 인스턴스화-> 인스턴스(객체)
+    Car("v8 engine","big")
+    val bigCar = Car("v8 engine","big")
+    //우리가 만든 클래스는 자료형이 된다.
+    val bigCar1:Car = Car("v8 engine","big")
+    val number :Int =10
+    var number1 = 20
+    val superCar:SuperCar = SuperCar("good engine","big","white")
+}
+
+//클래스(설명서) 만드는 방법(1)
+class Car(var engine:String,body:String){}
+
+//클래스(설명서) 만드는 방법(2)
+class SuperCar{
+    var engine : String = ""
+    var body : String = ""
+    var door : String = ""
+
+    constructor(engine : String,body:String,door:String)
+    {   println(engine)
+        println(body)
+        println(door)
+
+        this.engine = engine
+        this.body = body
+        this.door = door
+    }
+}
+fun abc(title:String,book:String)
+{
+
+}
+// 클래스(설명서) 만드는 방법(3)->1번 방법의 확장
+class Car1(engine: String,body: String)
+{   var door :String = ""
+
+    // 생성자
+    constructor(engine: String,body: String,door: String): this(engine,body){ // this= Car1
+        this.door = door
+    }
 
 }
