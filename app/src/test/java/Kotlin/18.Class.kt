c59917a5f5cb0dbd73310cffd20b21e681694c45
sws2 <ahnjh1028@naver.com>
2020-11-02 23:08:43 +0900

    type1

--- app/src/test/java/Kotlin/18.Class.kt
+++ app/src/test/java/Kotlin/18.Class.kt
@@ -1,7 +1,20 @@
 package Kotlin
 
+import java.util.ArrayList
+
 
 //18.Class
 // OOP-> Object Oriented Programing(객체지향 프로그래밍)
 //객체랑 뭘까?
 //- 이름이 있는 뭐든 것
+// 절차 지향 프로그래밍 : 코드를 위에서 부터 아래로 실행 하면 문제 해결
+//객체 지향 프로그래밍: 객체를 만들어서 객체에게 일을 시켜서 문제를 해결한다
+//선수 ,  심판, 경기장, 관중, -> 축구 게임
+
+
+//객체를 만드는 방법
+// - 설명서가 있어야 한다
+fun main(array:Array<String>)
+{
+
+}
