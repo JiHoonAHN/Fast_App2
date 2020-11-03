f4dddd1c960a79dcb44ec4ea112e536ebe6d0299
sws2 <ahnjh1028@naver.com>
2020-11-03 23:43:13 +0900

    commit

---
+++ app/src/test/java/Kotlin/10.Control_Flow(2).kt
@@ -0,0 +1,27 @@
+package Kotlin
+
+//10. 제어 흐름
+
+//When
+
+fun main(args:Array<String>)
+{
+    val value : Int = 1
+
+    when(value){
+        1 -> {println("Value is 1")
+        }
+        2-> println("Value is 2")
+        3->println("Value is 3")
+        else-> println("Value is 4")
+
+    }
+    val value2 =when(value)
+    {
+        1->10
+        2->20
+        3->30
+        else->100
+    }
+    println(value2)
+}
