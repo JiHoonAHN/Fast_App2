346d0ca0ec1a74976abdffed7827599e6aeb0eb4
sws2 <ahnjh1028@naver.com>
2020-11-03 23:16:11 +0900

    commit

--- app/src/test/java/Kotlin/18.Class.kt
+++ app/src/test/java/Kotlin/18.Class.kt
@@ -10,32 +10,41 @@ import java.util.ArrayList
 // 절차 지향 프로그래밍 : 코드를 위에서 부터 아래로 실행 하면 문제 해결
 //객체 지향 프로그래밍: 객체를 만들어서 객체에게 일을 시켜서 문제를 해결한다
 //선수 ,  심판, 경기장, 관중, -> 축구 게임
-
-
+//
+//
 //객체를 만드는 방법
 // - 설명서가 있어야 한다
 fun main(array:Array<String>)
 {
     //클래스(설명서)를 통해서 실체를 만드는 방법
     //-> 인스턴스화-> 인스턴스(객체)
-//    Car("v8 engine","big")
-//    val bigCar = Car("v8 engine","big")
-//    //우리가 만든 클래스는 자료형이 된다.
-//    val bigCar1:Car = Car("v8 engine","big")
-//    val number :Int =10
-//    var number1 = 20
-//    val superCar:SuperCar = SuperCar("good engine","big","white")
+    Car("v8 engine","big")
+    val bigCar = Car("v8 engine","big")
+    //우리가 만든 클래스는 자료형이 된다.
+    //val bigCar1:Car = Car("v8 engine","big")
+   // val number :Int =10
+    //var number1 = 20
+    //인스턴스가 가지고 있는 기능을 사용하는 방법
+    //val superCar:SuperCar = SuperCar("good engine","big","white")
+//    val runableCar:RunableCar= RunableCar("simple engine","short body")
+//    runableCar.drive()
+//    runableCar.ride()
+//    runableCar.navi("부산")
+    //인스턴스의 맴버 변수에 접근 하는 방법
+    val runableCar2:RunableCar2= RunableCar2("nice engine","long body")
+    println(runableCar2.body)
+    println(runableCar2.engine)
 }
 
 //클래스(설명서) 만드는 방법(1)
-//class Car(var engine:String,body:String){}
+class Car(var engine:String,body:String){}
 
 //클래스(설명서) 만드는 방법(2)
-class SuperCar{
-    var engine : String = ""
-    var body : String = ""
-    var door : String = ""
-
+//class SuperCar{
+//    var engine : String = ""
+//    var body : String = ""
+//    var door : String = ""
+//
 //    constructor(engine : String,body:String,door:String)
 //    {   println(engine)
 //        println(body)
@@ -45,7 +54,7 @@ class SuperCar{
 //        this.body = body
 //        this.door = door
 //    }
-}
+//}
 //fun abc(title:String,book:String)
 //{
 //
@@ -76,5 +85,37 @@ class Car2 {
         this.engine=engine
         this.body=body
         this.door=door
+    }
+}
+//class RunableCar(engine:String, body:String)
+//{
+//    fun ride() {
+//        println("탑승하였습니다.")
+//
+//    }
+//    fun drive(){
+//        println("달립니다 !")
+//    }
+//    fun navi(destination:String)
+//    {
+//        println("$destination 으로 목적지가 설정되었습니다.")
+//    }
+//}
+class  RunableCar2{
+    var engine:String = ""
+    var body:String =""
+    constructor(engine:String,body:String) {
+        this.engine = engine
+        this.body = body
+    }
+    fun ride() {
+        println("탑승하였습니다.")
+    }
+    fun drive(){
+        println("달립니다 !")
+    }
+    fun navi(destination:String)
+    {
+        println("$destination 으로 목적지가 설정되었습니다.")
     }
 }
