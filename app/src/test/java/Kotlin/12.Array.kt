189f947839bdb5334db6cbfcd53e1cdc054ca074
sws2 <ahnjh1028@naver.com>
2020-11-08 13:51:55 +0900

    commit

--- app/src/test/java/Kotlin/12.Array.kt
+++ app/src/test/java/Kotlin/12.Array.kt
@@ -8,16 +8,29 @@ fun main(array:Array<String>)
 {
     //배열 생성하는 방법(1)
     var number :Int = 10
-    var group1 = arrayOf<Int>()//<타입>을 적지 않으면 아무런 배열도 계속 넣을수 있다.
+    var group1 = arrayOf<Int>(1,2,3,4,5)//<타입>을 적지 않으면 아무런 배열도 계속 넣을수 있다.
     println(group1 is Array)
     //배열을 생성하는 방법(2)
     var number1 =10
     var group2 = arrayOf(1,2,3,4,5,"Hello")
-
+//Index란
+    // - > 순서(번째)
+    //[1,2,3,4,5]
+    //"0"부터 시작
+    //index0 -> 1, index 1 -> 2
 //배열의 값을 꺼내는 방법(1)
-    group1.get()
-
-
+    val test1 = group1.get(0)
+    val test2 = group1.get(4)
+    println(test1)
+//배열의 값을 꺼내는 방법(2)
+    val test3 = group1[0]
+    println(test3)
+//배열의 값을 바꾸는 방법(1)
+    group1.set(0,100)
+    println(group1[0])
+    //배열의 값을 바꾸는 방법(2)
+    group1[0]= 200
+    println(group1[0])
 
 }
 
