e65ae7a68aba7ed95818c8af3dc9ac031f414e1d
sws2 <ahnjh1028@naver.com>
2020-11-10 23:03:46 +0900

    lterable study(by kotlin)

--- app/src/test/java/Kotlin/16. Iterable.kt
+++ app/src/test/java/Kotlin/16. Iterable.kt
@@ -34,7 +34,34 @@ fun main(array:Array<String>)
     a.forEach { item->// 변수명 : it 이 마음에 안들때 사용
         println(item)
     }
+    //반복하는 방법(5)
     a.forEachIndexed { index, item ->
         println("index: "+index+"value: "+item)
+    }
+    //반복하는 방법(6)
+    println(a.size)// 9
+    println()
+    for(i in 0 until  a. size){
+        //until은 마지막을 포함하지 않는다.
+        //0부터 8까지 이다.
+        println(a.get(i))
+    }
+
+        //반복하는 방법(7)
+    for(i in 0 until a.size step (2)){
+        println(a.get(i))
+    }
+    println()
+
+    //반복하는 방법
+    for (i in a.size - 1 downTo (0))
+    {
+        //8부터 0까지 반복
+        println(a.get(i))
+    }
+
+    // 반복하는 방법(9)
+    for (i in a.size -1 downTo(0) step(2)){
+        println(a.get(i))
     }
 }
