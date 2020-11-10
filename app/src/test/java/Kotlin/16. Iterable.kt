00ef2bc4cf0b095cd1cfdddd8bca5f2505add18c
sws2 <ahnjh1028@naver.com>
2020-11-10 23:42:22 +0900

    lterable study(by kotlin)

--- app/src/test/java/Kotlin/16. Iterable.kt
+++ app/src/test/java/Kotlin/16. Iterable.kt
@@ -69,6 +69,19 @@ fun main(array:Array<String>)
     for (i in 0 .. a.size){
         // .. -> 마지막을 포함한다.
         println(i)
-        
     }
+    println()
+
+    //반복하는 방법(11)
+    var b: Int = 0// -> 1 -> 2 -> 3 -> 4
+    var c: Int = 4
+    while(b < c){
+        b++ // while문을 정지 시키기 위한 코드
+        println("b")
+    }
+
+    //반복하는 방법(12)
+    do{
+        println("hello")
+    }while(b < c)//무조건 한번은 출력
 }
