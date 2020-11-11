20e7b4b4d4e613d77402b5e1d12d05c18197341d
sws2 <ahnjh1028@naver.com>
2020-11-11 22:55:07 +0900

    veriable_range study(by kotlin)

--- app/src/test/java/Kotlin/22.Veriable_range.kt
+++ app/src/test/java/Kotlin/22.Veriable_range.kt
@@ -1,15 +1,33 @@
 package Kotlin
+//변수의 접근 범위
+//1. 전역변수 -> 여기저기서 접근 가능한것
+//2. 지역 변수 -> 여기저기에서 접근 불가능
 
-var number100 : Int = 10
+var number100 : Int = 10//아무나 갔다가 변경 가능-> ★위험 최소한 설정 중요★
 
 fun main(arg:Array<String>) {
     println(number100)
 
-
+    val test = Test("홀길동")
+    test.name
 
 }
 
 
 class Test(var name:String ){
+
+    fun testFun(){
+        var birth : String = "2000/3/1"
+        name = " 홍길동"
+        number100 = 100
+        fun testFun2(){
+            var gender : String = "male"// 접근 범위 정해짐
+        }
+    }
+    fun testFun2(){
+        name
+        //birth -> 지역번수
+    }
+
 
 }
