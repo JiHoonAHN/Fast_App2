679bfb572f9e30ea650d4b66e9c28db85a88d3a5
sws2 <ahnjh1028@naver.com>
2020-11-12 23:19:31 +0900

    Access_modifire study(by kotlin)

--- app/src/test/java/Kotlin/23.Access_modifire.kt
+++ app/src/test/java/Kotlin/23.Access_modifire.kt
@@ -5,12 +5,17 @@ package Kotlin
 
 fun main(array:Array<String>){
     val testAcess:TestAcess = TestAcess("아무개")
-    testAcess.test()
+    // private 키워드 때문에 외부에서 접속 불가
+//    testAcess.test()
 //    println(testAcess.name)
 //    testAcess.name = "아무개 투"
 //    println(testAcess.name)
     val reward:Reward = Reward()
     reward.rewardAmount = 2000
+
+    val runningCar : RunningCar = RunningCar()
+    runningCar.runFast()
+//    runningCar.run()
 }
 
 class Reward(){
@@ -21,14 +26,23 @@ class TestAcess{
 
     constructor(name :String) {
         this.name = name
-
     }
     fun ChangeName(newName:String){
         this.name = newName
     }
-    fun test()
-    {
+
+    private fun test() {
         println("테스트")
     }
 
+
+}
+//private 쓰는 이유 - 외부에 공개하고 싶지 않은 코드를 가리기 위해서
+class RunningCar(){
+    fun runFast(){
+        run()
+    }
+    private fun run(){//외부와 내부 단절
+
+    }
 }
