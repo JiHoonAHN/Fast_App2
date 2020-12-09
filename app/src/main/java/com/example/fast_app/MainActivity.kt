bf436662868e4ffe339e0a76c6d26d50bf518d41
sws2 <ahnjh1028@naver.com>
2020-12-09 23:46:16 +0900

    UI 설명 (by kotlin)

--- app/src/main/java/com/example/fast_app/MainActivity.kt
+++ app/src/main/java/com/example/fast_app/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setContentView(R.layout.support_simple_spinner_dropdown_item)
+        setContentView(R.layout.scrollview_13)
     }
 }
